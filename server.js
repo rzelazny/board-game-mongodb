8b88ce6ef1ece1300d4c6711a30575c06b7e1988
Ryan Zelazny <RyanZelazny@gmail.com>
2020-12-27 12:45:34 -0500

    setting up sessions and passport

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require("express");
+const session = require('express-session');
 const logger = require("morgan");
 const mongoose = require("mongoose");
+var passport = require("./config/passport");
 //const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
+// Creating express app and configuring middleware needed for authentication
 const app = express();
 
 app.use(logger("dev"));
-
 //app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "games rule", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/boardgame", {
   useNewUrlParser: true,
